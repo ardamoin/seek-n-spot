8dbd90e92166cd7827c17836693dbe68beeeafc1
ardamoin <ardamoin2@gmail.com>
2023-07-13 22:34:55 -0400

    Capitalize keys in levelMaps object.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,24 +1,23 @@
-import Header from "./Header";
 import MapCard from "./MapCard";
 import levelMaps from "./Maps";
 
 const Homepage = () => {
   return (
     <div className="flex flex-wrap justify-center">
-      <Header />
-      
-      <h1 className="font-mono text-accent font-semibold text-2xl mt-5">Choose a map!</h1>
+      <h1 className="mt-5 font-mono text-2xl font-semibold text-accent">
+        Choose a map!
+      </h1>
 
-      <div className="flex flex-wrap gap-20 justify-center py-20">
-        <MapCard image={levelMaps.dreamcast} levelName={'Dreamcast'} />
-        <MapCard image={levelMaps.gamecube} levelName={'Gamecube'}/>
-        <MapCard image={levelMaps.n64} levelName={'N64'}/>
-        <MapCard image={levelMaps.ps1} levelName={'PS1'}/>
+      <div className="flex flex-wrap justify-center gap-20 py-20">
+        <MapCard image={levelMaps.Dreamcast} levelName={"Dreamcast"} />
+        <MapCard image={levelMaps.Gamecube} levelName={"Gamecube"} />
+        <MapCard image={levelMaps.N64} levelName={"N64"} />
+        <MapCard image={levelMaps.PS1} levelName={"PS1"} />
         <div className="h-0 basis-full" />
-        <MapCard image={levelMaps.ps2} levelName={'PS2'}/>
-        <MapCard image={levelMaps.ps4} levelName={'PS4'}/>
-        <MapCard image={levelMaps.xbox} levelName={'Xbox'}/>
-        <MapCard image={levelMaps.xbox360} levelName={'Xbox 360'}/>
+        <MapCard image={levelMaps.PS2} levelName={"PS2"} />
+        <MapCard image={levelMaps.PS4} levelName={"PS4"} />
+        <MapCard image={levelMaps.Xbox} levelName={"Xbox"} />
+        <MapCard image={levelMaps.Xbox360} levelName={"Xbox 360"} />
       </div>
     </div>
   );
--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,21 +1,21 @@
-import dreamcast from "../assets/maps/dreamcast.jpg";
-import gamecube from "../assets/maps/gamecube.jpg";
-import n64 from "../assets/maps/n64.jpg";
-import ps1 from "../assets/maps/ps1.jpg";
-import ps2 from "../assets/maps/ps2.jpg";
-import ps4 from "../assets/maps/ps4.jpg";
-import xbox from "../assets/maps/xbox.jpg";
-import xbox360 from "../assets/maps/xbox360.jpg";
+import Dreamcast from "../assets/maps/dreamcast.jpg";
+import Gamecube from "../assets/maps/gamecube.jpg";
+import N64 from "../assets/maps/n64.jpg";
+import PS1 from "../assets/maps/ps1.jpg";
+import PS2 from "../assets/maps/ps2.jpg";
+import PS4 from "../assets/maps/ps4.jpg";
+import Xbox from "../assets/maps/xbox.jpg";
+import Xbox360 from "../assets/maps/xbox360.jpg";
 
 let levelMaps = {
-  dreamcast,
-  gamecube,
-  n64,
-  ps1,
-  ps2,
-  ps4,
-  xbox,
-  xbox360,
+  Dreamcast,
+  Gamecube,
+  N64,
+  PS1,
+  PS2,
+  PS4,
+  Xbox,
+  Xbox360,
 };
 
 export default levelMaps;
